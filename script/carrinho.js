ecb856337b280c0eef0d007d8036223220911b59
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2022-01-05 12:53:07 -0400

    começando a introdução dos elementos no carrinho

---
+++ script/carrinho.js
@@ -0,0 +1,43 @@
+let allProducts = [
+    {
+        name: 'Hot Holl',
+        price: '20',
+        image: '../../img/img-produtos/hot-roll.svg'
+    },
+    {
+        name: 'California',
+        price: '30',
+        image: '../../img/img-produtos/california.svg'
+    },
+    {
+        name: 'Makis',
+        price: '10',
+        image: '../../img/img-produtos/makis.svg'
+    },
+    {
+        name: 'Oshizushi',
+        price: '10',
+        image: '../../img/img-produtos/oshizushi.svg'
+    },
+    {
+        name: 'Rainbow Rolls',
+        price: '20',
+        image: '../../img/img-produtos/rainbow-rolls.svg'
+    },
+    {
+        name: 'Sashimi',
+        price: '30',
+        image: '../../img/img-produtos/sashimi.svg'
+    },
+    {
+        name: 'Spring Rolls',
+        price: '40',
+        image: '../../img/img-produtos/spring-rolls.svg'
+    },
+    {
+        name: 'Sushi',
+        price: '25',
+        image: '../../img/img-produtos/california.svg'
+    }
+]
+
